e97d42a3a634eb3a6a079a9c2016033f83659caa
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-07-28 12:28:31 +0300

    add conftest

---
+++ conftest.py
@@ -0,0 +1,51 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene.support.shared import browser
+
+from utils import attach
+from dotenv import load_dotenv
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
+
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    browser.config.base_url = "https://demoqa.com"
+    browser.driver.maximize_window()
+    browser.config.timeout = 10
+
+    yield
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
