b68d223d27d4ad5c6c116b39556ebbedf1d0cbb8
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-08-01 12:17:30 +0300

    change test_registration_form.py

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -12,7 +12,7 @@ from pages.registration_page import RegistrationPage
 @allure.link("http://demoqa.com/automation-practice-form", name="Testing")
 def test_registration_form():
     registration_page = RegistrationPage()
-    est_user = User()
+    test_user = User()
 
     with allure.step("Открыть форму регистрации студента"):
         registration_page.open()
